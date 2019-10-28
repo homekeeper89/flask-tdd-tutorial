c6e1f8aacf9db2be08e976bc7cf066fdce97d73c
김장훈 <homekeeper@gdnet.co.kr>
2019-10-28 16:25:43 +0900

    [fix] 깃 이그노어 정리

--- application/create.py
+++ application/create.py
@@ -5,9 +5,10 @@ from flask import Flask
 
 def create_app(mode='dev'):
     app = Flask(__name__)
-    if mode == 'dev':
-        app.config['DEBUG'] = True
     
+    from config import config_by_name
+    app.config.from_object(config_by_name[mode])
+
     from .src.views import api_user
     app.register_blueprint(api_user)
     return app
--- application/src/tests/test_server.py
+++ application/src/tests/test_server.py
@@ -1,6 +1,17 @@
 #  application/src/tests/
 # 서버가 켜지는지 확인 하는 테스트 코드
+from . import *
 
 def test_server_run(flask_app):
     app = flask_app
     assert app is not None
+
+@pytest.mark.parametrize('env, expected',
+        [('dev', 'Development'),
+        ('prod', 'Product'),
+        ('test', 'Testing')
+        ]
+)
+def test_server_env(env, expected):
+    app = create_app(env)
+    assert app.config['MESSAGE'] == expected
